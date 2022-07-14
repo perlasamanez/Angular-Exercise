ee8a7bc1d7a7db434ef77110e4b98ad13d3cb6e4
Perla Del Castillo <perlitadelcastillo@gmail.com>
2022-07-14 00:04:54 -0400

    [Component Setup] Added main parent Landing component and routing for the Landing page to appear first.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LandingComponent } from './modules/App/Landing/landing.component';
+
+const routes: Routes = [
+  { path: '', component: LandingComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './modules/App/app.component';
-import { LandingNavigationComponent } from './modules/App/Landing/LandingNavigation/landing-navigation.component';
-import { LandingIntroComponent } from './modules/App/Landing/LandingIntro/landing-intro.component';
+import { LandingComponent } from './modules/App/Landing/landing.component';
 import { LandingHowItWorksComponent } from './modules/App/Landing/LandingHowItWorks/landing-how-it-works.component';
+import { LandingIntroComponent } from './modules/App/Landing/LandingIntro/landing-intro.component';
+import { LandingNavigationComponent } from './modules/App/Landing/LandingNavigation/landing-navigation.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingNavigationComponent,
+    LandingComponent,
+    LandingHowItWorksComponent,
     LandingIntroComponent,
-    LandingHowItWorksComponent
+    LandingNavigationComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
