c776466829882d5a15bf7c8c1f365759268db842
Perla Del Castillo <perlitadelcastillo@gmail.com>
2022-07-15 02:47:42 -0400

    [Component Update] Added data from Rest API to display on the How It Works section. Also, added final style to this page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './modules/App/app.component';
 import { LandingComponent } from './modules/App/Landing/landing.component';
@@ -19,7 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/App/Landing/LandingHowItWorks/landing-how-it-works.component.ts
+++ src/app/modules/App/Landing/LandingHowItWorks/landing-how-it-works.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-landing-how-it-works',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingHowItWorksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  steps: any = [];
+  stepsInformation: any = [];
 
   ngOnInit(): void {
+    this.getSteps();
   }
 
+  getSteps() {
+    const url='https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge';
+    this._http.get(url).subscribe(
+      (response) => {
+        this.steps = response;
+        this.steps.sort(function(a: any, b: any) { return a.stepNumber - b.stepNumber });
+        this.getLatestModifiedStepsInformation();
+      }
+    );
+  }
+
+  getLatestModifiedStepsInformation() {
+    this.steps.forEach((step: any) => {
+      this.stepsInformation.push(step.versionContent.reduce((a: any, b: any) => (a.effectiveDate > b.effectiveDate ? a : b)));
+    });
+  }
 }
